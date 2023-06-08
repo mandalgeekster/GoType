bde5faacf5a8fd4e07566c9c2ffc3c5d0b927c40
Yashgaur0001 <yash.gaur0001@gmail.com>
2023-06-09 03:19:39 +0530

    shifting color

--- app.js
+++ app.js
@@ -3,73 +3,72 @@ var x;
 var words = paragraph.split(" ");
 var b="";   
 var index=0;
-console.log(words); 
-
+// console.log(words); 
 var i=0,j=0,count=0; 
+
+document.querySelector(".reset").addEventListener("click",function(event){
+    i=0;
+    j=0;
+    count=0;
+    index=0;
+    b="";
+    document.querySelector(".text").value="";
+    colorchange("white");
+});
+
 document.querySelector(".text").addEventListener("keypress",function(event){
     x=event.key
-    console.log(x);
     check(x);
 });
 
+function colorchange(str)
+{
+    var modifiedContent = "";
+    for(var a=0;a<words.length;a++)
+    {
+        if(index==a)
+        {
+            var span = document.createElement("span");
+            span.style.color = str;
+            span.textContent = words[a];
+            modifiedContent += span.outerHTML + " ";
+        }
+        else
+        {
+            modifiedContent += words[a] + " ";
+        }
+    }
+    // words[i]=modifiedContent;
+    document.querySelector(".one").innerHTML = modifiedContent.trim();
+}
+
+colorchange("grey");
 function check(letter)
 {
     if(x==" ")
     {
+        
         if(count==words[i].length)
         {
-            console.log(b);
-            var modifiedContent = "";
-            for(var a=0;a<words.length;a++)
-            {
-                if(index==a && words[a]==b)
-                {
-                    console.log(words[a],b);
-                    var span = document.createElement("span");
-                    span.style.color = "green";
-                    span.textContent = words[a];
-                    modifiedContent += span.outerHTML + " ";
-                }
-                else
-                {
-                    modifiedContent += words[a] + " ";
-                }
-            }
-            document.querySelector(".one").innerHTML = modifiedContent.trim();
-            
+            colorchange("green");
         }   
+        
         b="";
         i++;
         index=i;
+        colorchange("grey");
+        console.log(words);
         j=0;
         count=0;
-    }
+        }
     else if(x==words[i][j])
     { 
         b+=x;
-        console.log(x);
         j++;
         count++;   
     }
     else
     {
-        // b+=x;
-        var modifiedContent = "";
-        for(var a=0;a<words.length;a++)
-        {
-            if(index==a)
-            {
-                console.log(words[a],b);
-                var span = document.createElement("span");
-                span.style.color = "red";
-                span.textContent = words[a];
-                modifiedContent += span.outerHTML + " ";
-            }
-            else
-            {
-                modifiedContent += words[a] + " ";
-            }
-        }
-        document.querySelector(".one").innerHTML = modifiedContent.trim();
+        colorchange("red");
     }
 }
